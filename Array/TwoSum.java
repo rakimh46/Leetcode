1f09c9199c123c70e9bb4ee8282b517361daa0ed
rakimh46 <rakimh46@gmail.com>
2022-04-17 11:27:40 +0530

    array

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,1 +1,12 @@
-import java.util.*;
+public class TwoSum {
+
+    public static void main(String[] args) {
+        // int[] nums={2,7,11,15};
+        // int target=9;
+        int[] arr= twoSum(new int[] {2,7,11,15},9);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
